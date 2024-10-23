3e10dd33f5e05d513e70fcf2ffa13c7dc50bc0e0
whispperrrr <2130298079@qq.com>
2024-10-23 17:10:43 +0800

    删

--- miniprogram-2/pages/newmessages/newmessages.js
+++ miniprogram-2/pages/newmessages/newmessages.js
@@ -1,23 +1,11 @@
 Page({
   data: {
     messages: [{
-        icon: "/images1/image_newmessage/消息.png",
-        title: "通知消息",
-        detail: "暂无通知消息",
-        bgColor: "#FFD700"
-      },
-      {
-        icon: "/images1/image_newmessage/消息.png",
-        title: "互动消息",
-        detail: "暂无互动消息",
-        bgColor: "#FFA07A"
-      },
-      {
-        avatar: "/images1/image_newmessage/消息.png",
-        title: "甜甜",
-        detail: "还有剩余的小米手机没？"
-      }
-    ]
+      icon: "/images1/image_newmessage/消息.png",
+      title: "通知消息",
+      detail: "暂无通知消息",
+      bgColor: "#FFD700"
+    }]
   },
 
   onLoad: function () {
