cad5baedeb381cfb8b6878976b1f43096210e0bc
whs <778512790@qq.com>
2024-12-16 16:46:13 +0800

    加入心愿单

--- miniprogram-2/pages/goods/details/components/buy-bar/index.js
+++ miniprogram-2/pages/goods/details/components/buy-bar/index.js
@@ -6,27 +6,26 @@ Component({
   },
 
   properties: {
-    soldout: {
-      // 商品是否下架
+    soldout: { // 商品是否下架
       type: Boolean,
       value: false,
     },
-    isStock: {
+    isStock: { // 是否有库存
       type: Boolean,
       value: true,
-    }, // 是否有库存
-    isSlotButton: {
+    }, 
+    isSlotButton: { // 是否开启按钮插槽
       type: Boolean,
       value: false,
-    }, // 是否开启按钮插槽
-    minDiscountPrice: {
-      type: String,
-      value: '',
-    },
+    }, 
     SalePrice: {
       type: String,
       value: '',
     },
+    details:{ // 商品详情
+      type: Object,
+      value: {}
+    } 
   },
 
   data: {
@@ -46,6 +45,16 @@ Component({
       })
     },
 
+    toAddWishList(e) {
+      console.log("加入心愿单");
+    
+      // 将 goodsList 存储在本地缓存中
+      const goodsList = JSON.stringify(this.properties.details);
+      wx.setStorageSync('goodsList', goodsList);
+    
+      this.triggerEvent('toAddWishList', { details }); // 触发自定义事件，传递商品详情
+    },
+
     toBuyNow(e) {
       const {
         isStock
@@ -54,14 +63,5 @@ Component({
       this.triggerEvent('toBuyNow', e);
     },
 
-    toNav(e) {
-      const {
-        url
-      } = e.currentTarget.dataset;
-      return this.triggerEvent('toNav', {
-        e,
-        url,
-      });
-    },
   },
 });
